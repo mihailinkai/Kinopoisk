ee3bb377f60849d13eb630ca4e2a33d6bb262650
mihailinkai <misha.izgareva@gmail.com>
2019-06-18 21:06:19 +0300

    finish request

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 const searchForm = document.querySelector('#search-form');
-
+const movie = document.querySelector('#movies');
 function apiSearch(event){
     event.preventDefault();
   const searchText = document.querySelector('.form-control').value,
-  server = 'https://www.themoviedb.org/'; // можно считать что api это БД
-  console.log(requestApi(server));
+  server = 'http://api.themoviedb.org/3/search/multi?api_key=db17053ce28cd767b4e2926db21605bd&language=ru&query=' +
+  
+  searchText; // можно считать что api это БД
+ requestApi('GET', server);
 }
 
 searchForm.addEventListener('submit', apiSearch);
-function requestApi(url){
-return url;
+function requestApi(method, url){
 
+    const request = new XMLHttpRequest();
+    request.open(method, url);
+   request.send();
+
+   request.addEventListener('readystatechange', () => {
+if (request.readyState !== 4) return;
+
+
+if(request.status !== 200) {
+    console.log('error: ' + request.status);
+return;
+}
+const output = JSON.parse(request.responseText);
+
+let inner = '';
+
+output.results.forEach(function(item){
+let nameItem = item.name || item.title;
+console.log(nameItem);
+inner += inner + '<div class="col-3">' + nameItem + '</div>';
+});
+
+movie.innerHTML = inner;
+
+
+
+});
 
 }
